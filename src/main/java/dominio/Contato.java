b94dfd5c4bb16ff8393dce31ccaf130b5e781fa0
victorgustavo <victor_gustavo_2008@hotmail.com>
2016-10-19 11:03:55 -0300

    Dominios com Anotações

--- src/main/java/dominio/Contato.java
+++ src/main/java/dominio/Contato.java
@@ -1,20 +1,56 @@
 package dominio;
 
+/* Os imports da Persistencia DAO */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="contato")
 public class Contato {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_ATIVIDADE")
+	@SequenceGenerator(name="SEQ_ATIVIDADE", sequenceName="id_seq_atividade", allocationSize=1)
+	@Column(name="id_contato")
+	private String id;
+	
+	@Column(name= "nome_contato",columnDefinition="varchar(200)", nullable=false)
 	private String nome;
+	
+	@Column(name= "email_contato",columnDefinition="varchar(200)", nullable=false)
 	private String email;
+	
+	@Column(name= "telefone_contato",columnDefinition="varchar(50)", nullable=true)
 	private String telefone;
+	
+	@Column(name= "dataCadastro_contato",columnDefinition="timestamp without time zone", nullable=false)
+	private String dataCadastro;
+	
+	
+	/* Construtores da class */
 	
 	public Contato() {
 		// Construtor default
 	}
 	
-	public Contato(String nome, String email, String telefone) {
+	public Contato(String nome, String email, String telefone, String dataCadastro) {
 		this.nome = nome;
 		this.email = email;
 		this.telefone = telefone;
+		this.dataCadastro = dataCadastro;
 	}
 
+	
+	
+	/* Metodos da class */
+	
+	
 	public String getNome() {
 		return nome;
 	}
@@ -37,5 +73,21 @@ public class Contato {
 	
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(String dataCadastro) {
+		this.dataCadastro = dataCadastro;
 	}
 }
